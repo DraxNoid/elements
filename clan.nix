35b23669d4c9ed8eb4e5ff3f8f5abe6ee766dc61
clan-tool <clan@example.com>
2026-04-06 01:11:54 +0200

    feat: fill in name and domain for clan

--- clan.nix
+++ clan.nix
@@ -1,7 +1,7 @@
 {
   # Ensure this is unique among all clans you want to use.
-  meta.name = "__CHANGE_ME__";
-  meta.domain = "changeme";
+  meta.name = "elements";
+  meta.domain = "elements.internal";
 
   inventory.machines = {
     # Define machines here.
